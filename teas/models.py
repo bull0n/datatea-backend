412192c8edba43e77269670ee3e236dabaa60439
bull0n <lucas.bulloni@protonmail.ch>
2020-06-21 22:08:09 +0200

    complete model

--- teas/models.py
+++ teas/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
 class Tea(models.Model):
+
+    class Form(models.TextChoices):
+        BALL_ROLLED = 'ball_rolled', 'Ball Rolled'
+        LOOSE = 'loose', 'Loose'
+        PRESSED = 'pressed', 'Pressed'
+        POWDER = 'powdered', 'powdered'
+
     name = models.CharField(max_length=100)
+    year_in_school = models.CharField(max_length=20, choices=Form.choices)
+    would_buy_again = models.BooleanField()
+    price = models.FloatField(null=True, blank=True)
+    picking_season = models.DateField(null=True, blank=True)
+    origin = models.CharField(max_length=100, blank=True)
+    vendor = models.CharField(max_length=50, blank=True)
+    url_bought = models.CharField(max_length=200, blank=True)
+    vendor_description = models.TextField()
     description = models.TextField()
+
 
     def __str__(self):
         return self.name
