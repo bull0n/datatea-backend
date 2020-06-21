48525893ae3de1e32e45da7ffbe7a50736ee066a
bull0n <lucas.bulloni@protonmail.ch>
2020-06-21 21:53:27 +0200

    add a query for one tea

--- teas/schema.py
+++ teas/schema.py
@@ -12,5 +12,24 @@ class TeaType(DjangoObjectType):
 class Query(object):
     all_teas = graphene.List(TeaType)
 
+    tea = graphene.Field(TeaType,
+                        id=graphene.Int(),
+                        name=graphene.String())
+
+
     def resolve_all_teas(self, info, **kwargs):
         return Tea.objects.all()
+
+    def resolve_category(self, info, **kwargs):
+          id = kwargs.get('id')
+          name = kwargs.get('name')
+
+          if id is not None:
+              return Tea.objects.get(pk=id)
+
+          if name is not None:
+              return Tea.objects.get(name=name)
+
+          return None
+
+
